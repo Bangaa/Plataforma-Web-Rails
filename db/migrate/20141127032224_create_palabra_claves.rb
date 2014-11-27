b147c3ab3b21ebd4de73a7dda9ebfd8be1989605
Caro <caro@caro-Aspire-4741.(none)>
2014-11-27 01:34:37 -0300

    Se agregó atributos de tabla Palabra Clave

--- db/migrate/20141127032224_create_palabra_claves.rb
+++ db/migrate/20141127032224_create_palabra_claves.rb
@@ -1,6 +1,9 @@
 class CreatePalabraClaves < ActiveRecord::Migration
   def change
     create_table :palabra_claves do |t|
+    	t.integer	:pal_clave_id
+    	t.string	:palabra
+    
 
       t.timestamps null: false
     end
