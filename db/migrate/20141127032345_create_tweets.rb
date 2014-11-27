63465a5d355c5d37f099ee19382ce8287fa745f0
Caro <caro@caro-Aspire-4741.(none)>
2014-11-27 00:48:44 -0300

    Se agregó los atributos de tabla tweet

--- db/migrate/20141127032345_create_tweets.rb
+++ db/migrate/20141127032345_create_tweets.rb
@@ -1,6 +1,9 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
+    	t.integer 	:tweets_id
+    	t.datatime	:fecha
+    	t.string	:mensaje
 
       t.timestamps null: false
     end
