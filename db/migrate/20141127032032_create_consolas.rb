46daf018549ba94825566470812e850221dd565a
Caro <caro@caro-Aspire-4741.(none)>
2014-11-27 01:23:44 -0300

    Se agregó atributos de tabla Consola

--- db/migrate/20141127032032_create_consolas.rb
+++ db/migrate/20141127032032_create_consolas.rb
@@ -1,6 +1,9 @@
 class CreateConsolas < ActiveRecord::Migration
   def change
     create_table :consolas do |t|
+    	t.integer	:consola_id
+    	t.string	:nombre_consola
+    	t.string	:descripcion_consola
 
       t.timestamps null: false
     end
