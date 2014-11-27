1e181ae33d7eb12ec663b7916a9ce47d3cff0ac8
Caro <caro@caro-Aspire-4741.(none)>
2014-11-27 01:29:16 -0300

    Se agregó atributos de tabla Empresa

--- db/migrate/20141127032117_create_empresas.rb
+++ db/migrate/20141127032117_create_empresas.rb
@@ -1,6 +1,8 @@
 class CreateEmpresas < ActiveRecord::Migration
   def change
     create_table :empresas do |t|
+    	t.integer	:empresa_id
+    	t.string	:nombre
 
       t.timestamps null: false
     end
