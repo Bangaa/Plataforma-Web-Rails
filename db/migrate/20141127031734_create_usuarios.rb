1fa08eb601a3567374e748a0fb3be205ed0bb4b5
Ian Mejias <ian.mejias@usach.cl>
2014-11-27 01:28:56 -0300

    Se modifica el esquema de usuarios y estadisticas

--- db/migrate/20141127031734_create_usuarios.rb
+++ db/migrate/20141127031734_create_usuarios.rb
@@ -1,6 +1,9 @@
 class CreateUsuarios < ActiveRecord::Migration
   def change
     create_table :usuarios do |t|
+	  t.integer	:usuario_id
+	  t.string	:user_name
+	  t.integet	:seguidores
 
       t.timestamps null: false
     end
--- db/migrate/20141127032423_create_estadisticas.rb
+++ db/migrate/20141127032423_create_estadisticas.rb
@@ -1,6 +1,11 @@
 class CreateEstadisticas < ActiveRecord::Migration
   def change
     create_table :estadisticas do |t|
+	  t.integer	:estadistica_id
+	  t.integer	:positivos
+	  t.integer	:negativos
+	  t.integer	:neutrales
+	  t.integer	:suma_total
 
       t.timestamps null: false
     end
